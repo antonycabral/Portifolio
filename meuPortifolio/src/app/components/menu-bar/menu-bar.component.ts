c1ecb48e04a83db53b04477717934c612438f878
Antony Guilherme <83239872+antonycabral@users.noreply.github.com>
2024-09-29 16:11:18 -0300

    feat: responsividade

--- meuPortifolio/src/app/components/menu-bar/menu-bar.component.ts
+++ meuPortifolio/src/app/components/menu-bar/menu-bar.component.ts
@@ -1,18 +1,26 @@
+
+import { NgClass } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-menu-bar',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './menu-bar.component.html',
   styleUrl: './menu-bar.component.css'
 })
 export class MenuBarComponent
 {
+  menuActive = false;
+
+  toggleMenu() {
+    this.menuActive = !this.menuActive;
+  }
   scrollToSection(id: string): void {
     const element = document.getElementById(id);
     if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
     }
+    
 }
 }
